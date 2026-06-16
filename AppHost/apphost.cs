6e187a32d706de7b74b302537128036ecca5c7d7
Chris Usher <chrisushertester@gmail.com>
2026-06-16 18:22:06 +0100

    Get Aspire up and running

---
+++ AppHost/apphost.cs
@@ -0,0 +1,20 @@
+﻿#:sdk Aspire.AppHost.Sdk@13.2.4
+#:package Aspire.Hosting.Azure.Functions@13.2.4
+#:package Aspire.Hosting.Azure.Storage@13.2.4
+#:project ../Backend/Backend.csproj
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var storage = builder.AddAzureStorage("storage")
+    .RunAsEmulator(azurite =>
+    {
+        azurite.WithDataVolume("data");
+    });
+
+var api = builder.AddAzureFunctionsProject("Backend", "../Backend/Backend.csproj")
+    .WaitFor(storage)
+    .WithArgs("--verbose", "--script-root", @"..\..\..")
+    .WithHostStorage(storage)
+    .WithExternalHttpEndpoints();
+
+builder.Build().Run();
